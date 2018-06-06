93650a6e55abb15f74e6e455beac4f2c813eec80
Aiden-zyq <39584794+Aiden-zyq@users.noreply.github.com>
2018-06-06 18:09:38 +0800

    Update load_average.sh

--- script/load_average.sh
+++ script/load_average.sh
@@ -4,7 +4,7 @@
 #load average占用
 cpu_load_space () {
 local cpu_num=`grep 'model name' /proc/cpuinfo |wc -l` #统计cpu核数
-local load_average=`uptime | awk -F "," '{print $4}'|awk '{print $3}'` #load average取值
+local load_average=`uptime | awk -F ":" '{print $NF}'|awk -F ","'{print $3}'` #load average取值。
 declare cpu_value=`awk 'BEGIN {print $cpu_num*0.8}'` #报警阀值计算结果
 local value=80 #报警阀值
 local caveat="load average已达${cpu_value}" # 警告话语
