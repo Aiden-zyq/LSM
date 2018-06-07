6b593ffee4acb105ac9f263500011a322bf35cfc
Aiden-zyq <39584794+Aiden-zyq@users.noreply.github.com>
2018-06-07 16:55:52 +0800

    Update disk_used.sh

--- script/disk_used.sh
+++ script/disk_used.sh
@@ -1,1 +1,11 @@
-df -hx iso9660 | grep -v Filesystem |awk '{print 'NR',$1,int($5)}' #取磁盘分区文件系统和使用量的值
+#!/bin/bash
+#磁盘监控组
+
+#查询磁盘使用情况
+disk_used_space () {
+local disk_used=`df -hx iso9660 | grep -v Filesystem |awk '{print 'NR',$1,int($5)}'` #带行号打印磁盘文件系统和使用量
+local disk_value=`df -hx iso9660 | grep -v Filesystem |awk '{print 'NR',$1,int($5)}'|awk '{if($3>80) print $2}'` #取超出阀值的磁盘文件系统
+local value=80
+local caveat=""
+
+}
